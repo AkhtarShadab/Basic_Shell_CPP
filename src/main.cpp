84b977f725c46957db8d7efeefb7aba479f77885
AkhtarShadab <sakn501@gmail.com>
2024-06-13 14:09:11 +0530

    pass 1st stage

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,17 @@ int main() {
 
 
   // Uncomment this block to pass the first stage
-  while(1)
+  bool check = true;
+  while(check)
   {
     std::cout << "$ ";
   
     std::string input;
     std::getline(std::cin, input);
     std::cout<<input<<": command not found\n";
+    if(input=="exit")
+    {
+      check=false;
+    }
   }
 }
