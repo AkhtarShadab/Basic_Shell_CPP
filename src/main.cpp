1b09cbdeffe3182bdcdaa6b9ccb3786c4bd99288
AkhtarShadab <sakn501@gmail.com>
2024-06-13 19:30:29 +0530

    pass stage

--- src/main.cpp
+++ src/main.cpp
@@ -1,127 +1,78 @@
 #include <iostream>
-#include <vector>
 #include <string>
-#include <cstring>  // For strtok
-#include <cstdlib>  // For getenv
-#include <dirent.h> // For opendir, readdir, closedir
-#include <sys/stat.h> // For stat
-
-
-enum commands
+#include <sstream>
+#include <filesystem>
+using namespace std;
+enum validCommands
 {
-  echo,
-  cd,
-  quit,
-  type,
-  invalid
+    echo,
+    cd,
+    exit0,
+    type,
+    invalid,
 };
-
-
-
-std::vector<std::string> getPaths() {
-    std::vector<std::string> paths;
-    char* pathEnv = getenv("PATH");
-
-    if (pathEnv) {
-        char* pathCopy = strdup(pathEnv);
-        char* token = strtok(pathCopy, ":"); // Use ":" for Unix-like systems. Use ";" for Windows.
-
-        while (token != nullptr) {
-            paths.push_back(token);
-            token = strtok(nullptr, ":");
-        }
-
-        free(pathCopy);
-    }
-
-    return paths;
-}
-
-bool isExecutable(const std::string& filePath) {
-    struct stat sb;
-    if (stat(filePath.c_str(), &sb) == 0) {
-        return sb.st_mode & S_IXUSR;
-    }
-    return false;
-}
-
-
-commands string_to_command(std::string str)
-{
-  
-  std::string check;
-  int pos=str.find(" ");
-  if(pos==-1)
-  {
+validCommands isValid(std::string command){
+    command = command.substr(0,command.find(" "));
+    if(command == "echo") return validCommands::echo;
+    if(command == "cd") return validCommands::cd;
+    if(command == "exit") return validCommands::exit0;
+    if(command == "type") return validCommands::type;
     return invalid;
-  }
-  else{
-    check = str.substr(0,pos);
-    if(check=="exit") return quit;
-    if(check=="cd") return cd;
-    if(check=="type") return type;
-    if(check=="echo") return echo;
-  }
-  
-  return invalid;
 }
-std::string builtin_type(std::string str)
-{
-  std::string ans;
-  std::vector<std::string> paths=getPaths();
-  if(str=="echo"||str=="exit"||str=="type"||str=="cd")
-  {
-    ans=" is a shell builtin";
-    ans=str.append(ans);
-    return ans;
-  }
-  bool found = false;
-
-    for (const std::string& dir : paths) {
-        std::string fullPath = dir + "/" + str;
-
-        if (isExecutable(fullPath)) {
-            ans = " is ";
-            ans = ans.append(fullPath);
-            found=true;
+std::string valid[4] = {"echo", "cd", "exit0"};
+std::string get_path(std::string command){
+    std::string path_env = std::getenv("PATH");
+    std::stringstream ss(path_env);
+    std::string path;
+    while(!ss.eof()){
+        getline(ss, path, ':');
+        string abs_path = path + '/' + command;
+        if(filesystem::exists(abs_path)){
+            return abs_path;
         }
     }
-
-    if (!found) {
-        ans=": not found";
+    return "";  
+}
+int main() {
+    // You can use print statements as follows for debugging, they'll be visible when running tests.
+    // std::cout << "Logs from your program will appear here!\n";
+    bool exit = false;
+    while (!exit){
+        // Flush after every std::cout / std:cerr
+        std::cout << std::unitbuf;
+        std::cerr << std::unitbuf;
+        std::cout << "$ ";
+        std::string input;
+        std::getline(std::cin, input);
+        switch(isValid(input)){
+            case cd:
+                break;
+            case echo:
+                input.erase(0,input.find(" ")+1);
+                std::cout<<input<<"\n";
+                break;
+            case exit0:
+                exit=true;
+                break;
+            case type:
+                input.erase(0,input.find(" ")+1);
+                if(isValid(input) != invalid){
+                    std::cout<<input<<" is a shell builtin\n";
+                }
+                else{
+                    
+                    std::string path = get_path(input);
+                    if(path.empty()){
+                        std::cout<<input<<" not found\n";
+                    }
+                    else{
+                        std::cout<<input<<" is "<<path<<std::endl;
+                    }
+                }
+                break;
+            default:
+                std::cout<<input<<": command not found\n";
+                break;
+        }
     }
-    ans=str.append(ans);
-    return ans;
-} 
-int main()
-{
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
-  
-  std::string input = " ";
-  while(!input.empty())
-  {
-    std::string temp="";
-    std::cout << "$ ";
-    std::getline(std::cin, input);
-    
-    switch(string_to_command(input))
-    {
-    case echo:
-      std::cout << input.substr(5) << '\n';
-      break;
-    case quit:
-      return 0;
-    case type:
-      temp=input.substr(5);
-      temp = builtin_type(temp);
-      std::cout<<temp<<"\n";
-      break;
-    default:
-      std::cout << input << ": command not found" << '\n';
-      break;
-    }
-  }
-  return 0;
 }
