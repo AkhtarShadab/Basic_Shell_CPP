1071af49d96ea928a621d1591f881fdb632c0532
AkhtarShadab <sakn501@gmail.com>
2024-06-13 15:14:44 +0530

    pass 1st stage

--- src/main.cpp
+++ src/main.cpp
@@ -5,17 +5,39 @@ enum commands
   echo,
   cd,
   quit,
+  type,
   invalid
 };
 commands string_to_command(std::string str)
 {
   
-  if(str.find("echo") != std::string::npos) return echo;
-  if(str.find("cd") != std::string::npos) return cd;
-  if(str.find("exit") != std::string::npos) return quit;
+  std::string check;
+  int pos=str.find(" ");
+  if(pos==-1)
+  {
+    return invalid;
+  }
+  else{
+    check = str.substr(0,pos);
+    if(check=="exit") return quit;
+    if(check=="cd") return cd;
+    if(check=="type") return type;
+    if(check=="echo") return echo;
+  }
   
   return invalid;
 }
+std::string builtin_check(std::string str)
+{
+  std::string ans;
+  if(str=="echo"||str=="exit"||str=="type"||str=="cat"||str=="cd")
+  {
+    ans=" is a shell builtin";
+    ans=str.append(ans);
+  }
+  else ans="nonexistent: not found";
+  return ans;
+} 
 int main()
 {
   // Flush after every std::cout / std:cerr
@@ -25,6 +47,7 @@ int main()
   std::string input = " ";
   while(!input.empty())
   {
+    std::string temp="";
     std::cout << "$ ";
     std::getline(std::cin, input);
     
@@ -35,6 +58,11 @@ int main()
       break;
     case quit:
       return 0;
+    case type:
+      temp=input.substr(5);
+      temp = builtin_check(temp);
+      std::cout<<temp<<"\n";
+      break;
     default:
       std::cout << input << ": command not found" << '\n';
       break;
