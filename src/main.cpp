27261cccde4b61ca865eb0a7e5706a63cd8f34bc
AkhtarShadab <sakn501@gmail.com>
2024-12-17 20:41:14 +0000

    FinalNow

--- src/main.cpp
+++ src/main.cpp
@@ -1,159 +1,266 @@
-#include <iostream>
-#include <string>
-#include <sstream>
+#include <cstddef>
+#include <cstdlib>
 #include <filesystem>
+#include <iostream>
+#include <pthread.h>
 #include <vector>
-using namespace std;
-enum validCommands
+
+enum class CommandType
 {
-    echo,
-    cd,
-    exit0,
-    type,
-    pwd,
-    invalid,
+    BUILTIN,
+    EXECUTABLE,
+    ILLEGAL,
+    NONE,
 };
-validCommands isValid(std::string command) {
-    command = command.substr(0, command.find(" "));
-    if (command == "echo") return validCommands::echo;
-    if (command == "cd") return validCommands::cd;
-    if (command == "exit") return validCommands::exit0;
-    if (command == "type") return validCommands::type;
-    if (command == "pwd") return validCommands::pwd;
-    return invalid;
-}
-std::string valid[4] = {"echo", "cd", "exit0"};
-std::string get_path(std::string command) {
-    std::string path_env = std::getenv("PATH");
-    std::stringstream ss(path_env);
-    std::string path;
-    while (!ss.eof()) {
-        getline(ss, path, ':');
-        string abs_path = path + '/' + command;
-        if (filesystem::exists(abs_path)) {
-            return abs_path;
-        }
-    }
-    return "";
-}
-void get_info(std::string path)
+struct CommandToken
 {
-    try{
-    std::string targetPath;
-
-        if (path.empty() || path == "~") {
-            // Get the home directory from the HOME environment variable
-            const char* homeDir = getenv("HOME");
-            if (homeDir == nullptr) {
-                std::cerr << "cd: Could not determine home directory. HOME environment variable not set." << std::endl;
-                return;
-            }
-            targetPath = homeDir;
-        } else if (path[0] == '~') {
-            // Replace ~ with the home directory path
-            const char* homeDir = getenv("HOME");
-            if (homeDir == nullptr) {
-                std::cerr << "cd: Could not determine home directory. HOME environment variable not set." << std::endl;
-                return;
-            }
-            targetPath = std::string(homeDir) + path.substr(1); // Append the rest of the path
-        } else {
-            // Use the provided path as-is for relative and absolute paths
-            targetPath = path;
-        }
-
-        // Resolve to an absolute path
-        std::filesystem::path resolvedPath = std::filesystem::absolute(targetPath);
-
-        // Check if the target path exists and is a directory
-        if (!std::filesystem::exists(resolvedPath)) {
-            std::cerr << "cd: " << path << ": No such file or directory" << std::endl;
-            return;
-        }
-
-        if (!std::filesystem::is_directory(resolvedPath)) {
-            std::cerr << "cd: " << path << ": Not a directory" << std::endl;
-            return;
-        }
-
-        // Set the current path
-        std::filesystem::current_path(resolvedPath);
-    } catch (const std::filesystem::filesystem_error& e) {
-        std::cerr << "cd: " << path << ": " << e.what() << std::endl;
-    }
-}
-int main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    // std::cout << "Logs from your program will appear here!\n";
-
-    bool exit = false;
-    while (!exit) {
-        // Flush after every std::cout / std:cerr
-        std::cout << std::unitbuf;
-        std::cerr << std::unitbuf;
+    CommandType type;
+    // If type is EXECUTABLE, then lexeme will be the path
+    std::string lexeme;
+};
+std::vector<std::string> splitString(std::string, char = ' ');
+std::string getCommandPath(std::string);
+CommandToken tokenize(std::string);
+int main()
+{
+    // Flush after every std::cout / std:cerr
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
+    // Uncomment this block to pass the first stage
+    std::vector<std::string> inputBuffer;
+    while (true)
+    {
         std::cout << "$ ";
         std::string input;
         std::getline(std::cin, input);
-        std::istringstream iss(input);
-        std::string command, arg;
-        std::filesystem::path path;
-        switch (isValid(input)) {
-        case cd:
-            iss>>arg;
-            iss>>arg;
-            get_info(arg);
-            break;
-        case echo:
-            input.erase(0, input.find(" ") + 1);
-            std::cout << input << "\n";
-            break;
-        case exit0:
-            exit = true;
-            break;
-        case type:
-            input.erase(0, input.find(" ") + 1);
-            if (isValid(input) != invalid) {
-                std::cout << input << " is a shell builtin\n";
-            }
-            else {
-
-                std::string path = get_path(input);
-                if (path.empty()) {
-                    std::cout << input << ": not found\n";
+        inputBuffer = splitString(input);
+        CommandToken command = tokenize(inputBuffer[0]);
+        switch (command.type)
+        {
+        case CommandType::BUILTIN:
+            if (command.lexeme == "exit")
+            {
+                if (inputBuffer.size() > 1)
+                {
+                    return std::stoi(inputBuffer[1]);
                 }
-                else {
-                    std::cout << input << " is " << path << std::endl;
+                return 0;
+            }
+            else if (command.lexeme == "echo")
+            {
+                if (inputBuffer.size() > 1)
+                {
+                    std::string outputValue;
+                    for (int i = 1; i < inputBuffer.size() - 1; i++)
+                    {
+                        outputValue = inputBuffer[i];
+                        if (inputBuffer[i][0] == '\\')
+                        {
+                            outputValue = inputBuffer[i].substr(1);
+                        }
+                        if (inputBuffer[i][0] == '"' || inputBuffer[i][0] == '\'')
+                        {
+                            outputValue = inputBuffer[i].substr(1, inputBuffer[i].size() - 2);
+                        }
+                        std::cout << outputValue; // << ' ';
+                    }
+                    outputValue = inputBuffer[inputBuffer.size() - 1];
+                    if (inputBuffer[inputBuffer.size() - 1][0] == '\\')
+                    {
+                        outputValue = inputBuffer[inputBuffer.size() - 1].substr(1);
+                    }
+                    else if (inputBuffer[inputBuffer.size() - 1][0] == '"' ||
+                             inputBuffer[inputBuffer.size() - 1][0] == '\'')
+                    {
+                        outputValue = inputBuffer[inputBuffer.size() - 1].substr(
+                            1, inputBuffer[inputBuffer.size() - 1].size() - 2);
+                    }
+                    std::cout << outputValue << std::endl;
+                }
+                else
+                {
+                    std::cout << std::endl;
+                }
+            }
+            else if (command.lexeme == "type")
+            {
+                if (inputBuffer.size() > 1)
+                {
+                    CommandToken checkType = tokenize(inputBuffer[1]);
+                    switch (checkType.type)
+                    {
+                    case CommandType::BUILTIN:
+                        std::cout << checkType.lexeme << " is a shell builtin" << std::endl;
+                        break;
+                    case CommandType::EXECUTABLE:
+                        std::cout << inputBuffer[1] << " is " << checkType.lexeme
+                                  << std::endl;
+                        break;
+                    case CommandType::ILLEGAL:
+                        std::cout << checkType.lexeme << ": not found" << std::endl;
+                        break;
+                    case CommandType::NONE:
+                        std::cout << std::endl;
+                        break;
+                    }
+                }
+            }
+            else if (command.lexeme == "pwd")
+            {
+                std::string cwd = std::filesystem::current_path();
+                std::cout << cwd << std::endl;
+            }
+            else if (command.lexeme == "cd")
+            {
+                if (inputBuffer.size() > 1)
+                {
+                    if (inputBuffer[1] == "~")
+                    {
+                        std::filesystem::current_path(std::getenv("HOME"));
+                    }
+                    else if (std::filesystem::exists(inputBuffer[1]))
+                    {
+                        std::filesystem::current_path(inputBuffer[1]);
+                    }
+                    else
+                    {
+                        std::cout << inputBuffer[1] << ": No such file or directory"
+                                  << std::endl;
+                    }
+                }
+                else
+                {
+                    std::filesystem::current_path(std::getenv("HOME"));
                 }
             }
             break;
-        case pwd:
-            std::cout<<std::filesystem::current_path().string()<<"\n";
+        case CommandType::ILLEGAL:
+            std::cout << command.lexeme << ": command not found" << std::endl;
             break;
         default:
-            
-            
-            iss >> command;
-            std::vector<std::string> arguments;
-            while (iss >> arg)
+            std::cout << inputBuffer[0] << ": command not found" << std::endl;
+            break;
+        case CommandType::NONE:
+            break;
+        case CommandType::EXECUTABLE:
+            std::string executableCommand = command.lexeme + " ";
+            for (int i = 1; i < inputBuffer.size(); i++)
             {
-                arguments.push_back(arg);
-            }
 
-            std::string path = get_path(command);
-            if (path.empty())
-            {
-                std::cout << command << ": command not found\n";
+                executableCommand += inputBuffer[i];
             }
-            else
-            {
-                for (const auto& arg : arguments) {
-                    path += " " + arg;  // Append arguments to the command string
-                }
-                // std::cout<<p/ath;
-                int exitCode = std::system(path.c_str());
-            }
-
+            std::system(executableCommand.c_str());
             break;
         }
     }
+}
+std::vector<std::string> splitString(std::string inputString, char delimiter)
+{
+    std::vector<std::string> splitInputs;
+    if (inputString.empty())
+    {
+        return splitInputs = {""};
+    }
+    size_t index = 0;
+    std::string toInput = "";
+    while (index < inputString.size())
+    {
+        if (inputString[index] == '\\')
+        {
+            if (toInput.empty())
+            {
+                toInput += inputString[index];
+            }
+            index++;
+            toInput += inputString[index];
+            index++;
+        }
+        else if (inputString[index] == '\'' || inputString[index] == '"')
+        {
+            char quote = inputString[index++];
+            toInput = quote;
+            while (inputString[index] != quote)
+            {
+                if (inputString[index] == '\\')
+                {
+                    if (inputString[index + 1] == quote)
+                    {
+                        index++;
+                    }
+                    else if (quote == '"' && (inputString[index + 1] == '$' ||
+                                              inputString[index + 1] == '\\'))
+                    {
+                        index++;
+                    }
+                }
+                toInput += inputString[index++];
+            }
+            toInput += inputString[index++];
+            splitInputs.push_back(toInput);
+            toInput = "";
+        }
+        else if (inputString[index] == delimiter)
+        {
+            if (!toInput.empty())
+            {
+                splitInputs.push_back(toInput);
+            }
+            if (delimiter == ' ' && splitInputs.size() > 1 &&
+                splitInputs[splitInputs.size() - 1] != " ")
+            {
+                splitInputs.push_back(" ");
+            }
+            toInput = "";
+            index++;
+        }
+        else
+        {
+            toInput += inputString[index++];
+        }
+    }
+    if (!toInput.empty())
+    {
+        splitInputs.push_back(toInput);
+    }
+    return splitInputs;
+}
+std::string getCommandPath(std::string command)
+{
+    std::vector<std::string> paths = splitString(std::getenv("PATH"), ':');
+    std::string path;
+    for (int i = 0; i < paths.size(); i++)
+    {
+        path = paths[i] + '/' + command;
+        if (std::filesystem::exists(path))
+            return path;
+    }
+    return "";
+}
+CommandToken tokenize(std::string lexeme)
+{
+    CommandToken command;
+    command.lexeme = lexeme;
+    if (lexeme == "echo" || lexeme == "type" || lexeme == "exit" ||
+        lexeme == "pwd" || lexeme == "cd")
+    {
+        command.type = CommandType::BUILTIN;
+    }
+    else if (lexeme.empty())
+    {
+        command.type = CommandType::NONE;
+    }
+    else
+    {
+        std::string path = getCommandPath(lexeme);
+        if (!path.empty())
+        {
+            command.type = CommandType::EXECUTABLE;
+            command.lexeme = path;
+        }
+        else
+        {
+            command.type = CommandType::ILLEGAL;
+        }
+    }
+    return command;
 }
